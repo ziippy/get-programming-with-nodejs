ad6719865b0e7b08602e3fb511687b064ae1e7d6
ziippy <ziippy@naver.com>
2022-12-15 21:28:53 +0900

    파일 기반 route

--- 06_serve_html/main.js
+++ 06_serve_html/main.js
@@ -8,18 +8,67 @@ const routeMap = {
     "/": "views/index.html"
 };
 
-http.createServer((req, res) => {
-    res.writeHead(httpStatus.OK, {
+const sendErrorResponse = res => {
+    res.writeHead(httpStatus.StatusCodes.NOT_FOUND, {
         "Content-Type": "text/html"
     });
+    res.write("<h1>File Not Found!</h1>");
+    res.end();
+};
 
-    if (routeMap[req.url]) {
+const customReadFile = (file_path, res) => {
+    if (fs.existsSync(file_path)) {
         fs.readFile(routeMap[req.url], (error, data) => {
+            if (error) {
+                console.log(error);
+                sendErrorResponse(res);
+                return;
+            }
             res.write(data);
             res.end();
         });
     } else {
-        res.end("<h1>Sorry, not found.</h1>");
+        sendErrorResponse(res);
+    }
+};
+
+http.createServer((req, res) => {
+    // res.writeHead(httpStatus.StatusCodes.OK, {
+    //     "Content-Type": "text/html"
+    // });
+
+    // if (routeMap[req.url]) {
+    //     fs.readFile(routeMap[req.url], (error, data) => {
+    //         res.write(data);
+    //         res.end();
+    //     });
+    // } else {
+    //     res.end("<h1>Sorry, not found.</h1>");
+    // }
+
+    let url = req.url;
+    if (url.indexOf(".html") !== -1) {
+        res.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "text/html"
+        });
+        customReadFile(`./views${url}`, res);
+    } else if (url.indexOf(".js") !== -1) {
+        res.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "text/javascript"
+        });
+        customReadFile(`./public/js${url}`, res);
+    } else if (url.indexOf(".css") !== -1) {
+        res.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "text/css"
+        });
+        customReadFile(`./public/css${url}`, res);
+    } else if (url.indexOf(".png") !== -1) {
+        res.writeHead(httpStatus.StatusCodes.OK, {
+            "Content-Type": "image/png"
+        });
+        customReadFile(`./public/image${url}`, res);
+    } else {
+        sendErrorResponse(res);
     }
 }).listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
